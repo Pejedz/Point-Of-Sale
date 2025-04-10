a9433e38d7a5013a579d4febc7ea84ce14f5e5cb
Raihan Firza Fardianto <effzeed@gmail.com>
2025-04-11 00:30:15 +0700

    Integrasi Template

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
-
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TransactionDetailController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +13,10 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+Route::resource('/category', CategoryController::class);
+Route::resource('/item', ItemController::class);
+Route::resource('/transaction', TransactionController::class);
+Route::resource('/detail', TransactionDetailController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
